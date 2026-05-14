5455c431b869ec00118a8f8e4628c26845d8dcca
samuelevi <evisammy001@gmail.com>
2026-05-14 02:53:08 +0100

    new commit

--- resumes/resume.cs
+++ resumes/resume.cs
@@ -3,11 +3,9 @@ using System.Collections.Generic;
 
 public class Resume
 {
-    // Member variables
     public string _name;
     public List<Job> _jobs = new List<Job>();
 
-    // Method to display resume details
     public void Display()
     {
         Console.WriteLine($"Name: {_name}");
@@ -15,7 +13,7 @@ public class Resume
 
         foreach (Job job in _jobs)
         {
-            job.Display(); // Call the Job’s Display method
+            job.Display();
         }
     }
 }
