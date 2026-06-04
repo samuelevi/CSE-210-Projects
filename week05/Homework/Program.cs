283939ab13426cc419acf35aa06177d5d800c0bf
samuelevi <evisammy001@gmail.com>
2026-06-04 14:04:21 +0100

    week 05 update

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,9 +1,21 @@
 using System;
 
-class Program
+namespace Homework
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        static void Main(string[] args)
+        {
+            Assignment assignment1 = new Assignment("Samuel Bennett", "Multiplication");
+            Console.WriteLine(assignment1.GetSummary());
+
+            MathAssignment mathAssignment1 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+            Console.WriteLine(mathAssignment1.GetSummary());
+            Console.WriteLine(mathAssignment1.GetHomeworkList());
+
+            WritingAssignment writingAssignment1 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+            Console.WriteLine(writingAssignment1.GetSummary());
+            Console.WriteLine(writingAssignment1.GetWritingInformation());
+        }
     }
 }
