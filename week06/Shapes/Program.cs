ec8c5b311f3501a8c8d2eba5f3e638beb85ff888
samuelevi <evisammy001@gmail.com>
2026-06-12 01:35:00 +0100

    week06

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+namespace ShapesProject
 {
-    static void Main(string[] args)
+
+
+    class Program
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        static void Main(string[] args)
+        {
+            List<Shape> shapes = new List<Shape>();
+
+            shapes.Add(new Square("Red", 4));
+            shapes.Add(new Rectangle("Blue", 5, 10));
+            shapes.Add(new Circle("Green", 3));
+
+            foreach (Shape shape in shapes)
+            {
+                string color = shape.GetColor();
+                double area = shape.GetArea();
+
+                Console.WriteLine($"The {color} shape has an area of {area:F2}.");
+            }
+        }
     }
 }
